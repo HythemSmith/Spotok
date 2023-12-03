1eb8428faafc13657736908878f4abf3cb9e9b3f
VINH BÙI QUANG <21127723@student.hcmus.edu.vn>
2023-12-03 14:28:14 +0700

    Commit

---
+++ BE/database/models/mediaSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const MediaSchema = new Schema ({
+    userID: {
+        type: ObjectId
+    },
+    userName: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    gender: {
+        type: Boolean,
+        require: true
+    },
+    dateOfBirth: {
+        type: Date,
+        require: true
+    }
+}) 
--- BE/server.js
+++ BE/server.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const app = express()
+const db = require('./database/db.js')
+
+db.connect()
 
 app.listen(3000, () => console.log("Server is started"))
