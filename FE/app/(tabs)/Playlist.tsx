424931505c0772a73c4072a1caafe6fc253d1882
2eggsnoodles <thmnhat21@clc.fitus.edu.vn>
2023-12-10 23:57:57 +0700

    modified:   FE/app/(tabs)/Playlist.tsx 	modified:   FE/components/song.tsx

--- FE/app/(tabs)/Playlist.tsx
+++ FE/app/(tabs)/Playlist.tsx
@@ -9,122 +9,112 @@ import Song from '../../components/song'
 const PlaylistsScreen = () => {
     return (
         <ScrollView style={{ flex: 1, marginTop: 50 }}>
-          <Link href={{
-            pathname: "/(tabs)/",
-            }} asChild>
-            <Pressable>
-              <Ionicons name="arrow-back" size={24} color="white" />
-            </Pressable>
-          </Link>
+            <Link href={{
+                pathname: "/(tabs)/",
+                }} asChild>
+                <Pressable>
+                    <Ionicons name="arrow-back" size={24} color="white" />
+                </Pressable>
+            </Link>
           <Pressable
-            style={{
-              marginHorizontal: 10,
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-              marginTop: 9,
-            }}
-          >
-            <Pressable
               style={{
-                flexDirection: "row",
-                alignItems: "center",
-                gap: 10,
-                backgroundColor: "#42275a",
-                padding: 9,
-                flex: 1,
-                borderRadius: 3,
-                height: 38,
-              }}
-            >
-              <Ionicons name="search" size={20} color="white" />
-              <TextInput
-                placeholder="Find in playlist"
-                placeholderTextColor={"white"}
-                style={{ fontWeight: "500",color:"white" }}
-              />
-            </Pressable>
-            <Pressable
-              style={{
-                marginHorizontal: 10,
-                backgroundColor: "#42275a",
-                padding: 10,
-                borderRadius: 3,
-                height: 38,
-              }}
-            >
-              <Text style={{ color: "white" }}>Sort</Text>
-            </Pressable>
-          </Pressable>
-
-          <View style={{ height: 50 }} />
-          <View style={{ marginHorizontal: 10 }}>
-            <Text style={{ fontSize: 18, fontWeight: "bold", color: "white" }}>
-              Playlist
-            </Text>
-            <Text style={{ color: "white", fontSize: 13, marginTop: 5 }}>
-              20 songs
-            </Text>
-          </View>
-
-          <Pressable
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-              marginHorizontal: 10,
-            }}
-          >
-            <Pressable
-              style={{
-                width: 30,
-                height: 30,
-                borderRadius: 15,
-                backgroundColor: "#1DB954",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Ionicons name="arrow-down" size={20} color="white" />
-            </Pressable>
-
-            <View
-              style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
-            >
+                  marginHorizontal: 10,
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  marginTop: 9,
+              }}>
+              <Pressable
+                  style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                      gap: 10,
+                      backgroundColor: "#42275a",
+                      padding: 9,
+                      flex: 1,
+                      borderRadius: 3,
+                      height: 38,
+                  }}>
+                  <Ionicons name="search" size={20} color="white" />
+                  <TextInput
+                      placeholder="Find in playlist"
+                      placeholderTextColor={"white"}
+                      style={{ fontWeight: "500",color:"white" }}
+                  />
+              </Pressable>
               <Pressable
                 style={{
-                  width: 60,
-                  height: 60,
-                  borderRadius: 30,
+                  marginHorizontal: 10,
+                  backgroundColor: "#42275a",
+                  padding: 10,
+                  borderRadius: 3,
+                  height: 38,
+                }}>
+                  <Text style={{ color: "white" }}>Sort</Text>
+              </Pressable>
+          </Pressable>
+          <View style={{ height: 50 }} />
+          <View style={{ marginHorizontal: 10 }}>
+              <Text style={{ fontSize: 18, fontWeight: "bold", color: "white" }}>
+                  Playlist
+              </Text>
+              <Text style={{ color: "white", fontSize: 13, marginTop: 5 }}>
+                  20 songs
+              </Text>
+          </View>
+          <Pressable
+              style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  marginHorizontal: 10,
+              }}>
+              <Pressable
+                style={{
+                  width: 30,
+                  height: 30,
+                  borderRadius: 15,
+                  backgroundColor: "#1DB954",
                   justifyContent: "center",
                   alignItems: "center",
-                  backgroundColor: "#1DB954",
-                }}
-              >
-                <Ionicons name="caret-forward" size={24} color="white" />
+                }}>
+                  <Ionicons name="arrow-down" size={20} color="white" />
               </Pressable>
-            </View>
-        </Pressable>
-        <FlatList
-                    data={songs}
-                    renderItem={({item}) => (
-                      <Link href={{
-                        pathname: "/user/Songscreen",
-                        params: {
+
+              <View
+                  style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+                  <Pressable
+                      style={{
+                          width: 50,
+                          height: 50,
+                          borderRadius: 30,
+                          justifyContent: "center",
+                          alignItems: "center",
+                          backgroundColor: "#1DB954",
+                      }}>
+                      <Ionicons name="caret-forward" size={24} color="white" />
+                  </Pressable>
+              </View>
+          </Pressable>
+          <FlatList
+              data={songs}
+              renderItem={({item}) => (
+                  <Link href={{
+                      pathname: "/user/Songscreen",
+                      params: {
                           name: item.title,
                           image: item.image,
-                          artist: item.artist
-                        }
+                          artist: item.artist}
                       }} asChild>
-                        <Pressable>
+                      <Pressable>
                           <Song
                             name = {item.title}
                             image = {item.image}
                             artist = {item.artist}/>
-                        </Pressable>
-                      </Link>
-                    )}
-                />
+                      </Pressable>
+                  </Link>
+              )}
+          />
         </ScrollView>
     );
 }
--- FE/components/song.tsx
+++ FE/components/song.tsx
@@ -1,21 +1,19 @@
 import { StyleSheet, Text, View, Pressable, Image } from "react-native"
-import React ,{useContext} from  "react"
+import React from  "react"
 import { Ionicons } from "@expo/vector-icons"
-import { Stack, useLocalSearchParams } from "expo-router"
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Song = ({ name, image, artist}: { name: string; image: string; artist:string }) => {
   return (
     <SafeAreaView
-    style={{flex:1, flexDirection: "row", alignItems: "center", height: 100 }}
+    style={{flexWrap: "wrap", flexDirection: "row", height: 50 }}
     >
       <Image
       style={{ width: 50, height: 50, marginRight: 10 }}
       source={{uri:image as string}}
       />
-      <View style={{ flex: 1 }}>
+      <View style={{marginTop: 5, flex: 1, flexDirection: "column" }}>
         <Text
-          numberOfLines={1}
           style={{
                 fontWeight: "bold",
                 fontSize: 14,
@@ -33,6 +31,7 @@ const Song = ({ name, image, artist}: { name: string; image: string; artist:stri
         alignItems: "center",
         gap: 7,
         marginHorizontal: 10,
+        height: 50
         }}>
           <Ionicons name="heart" size={24} color="#1DB954"/>
           <Ionicons name="ellipsis-vertical" size={24} color="#C0C0C0" />
