1465474f5bced5b1ec6baac604ae31dbb6665da6
2eggsnoodles <thmnhat21@clc.fitus.edu.vn>
2023-12-11 00:00:55 +0700

    modified:   FE/app/user/News.tsx 	modified:   FE/app/user/Podcast.tsx 	modified:   FE/app/user/Short.tsx

--- FE/app/user/News.tsx
+++ FE/app/user/News.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import {Text, View, Image, SafeAreaView, TouchableOpacity, FlatList, ScrollView} from 'react-native';
 import styles from '../styles';
+import { Stack, useLocalSearchParams } from "expo-router"
 
 const NewsScreen = () => {
     return (
         <View style={styles.container}>
+            <Stack.Screen options={{
+                headerTitle: 'News'
+            }}/>
             <Text>News!</Text>
         </View>
     );
--- FE/app/user/Podcast.tsx
+++ FE/app/user/Podcast.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import {Text, View, Image, SafeAreaView, TouchableOpacity, FlatList, ScrollView} from 'react-native';
 import styles from '../styles';
+import { Stack, useLocalSearchParams } from "expo-router"
+
 
 const PodcastScreen = () => {
     return (
         <View style={styles.container}>
+            <Stack.Screen options={{
+            headerTitle: 'Podcast'
+            }}/>
             <Text>Podcast!</Text>
         </View>
     );
