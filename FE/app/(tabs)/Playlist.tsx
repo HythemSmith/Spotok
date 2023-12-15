a1aa2f8b3cc216c7b983f6527ab1c5ca0668cc6e
2eggsnoodles <thmnhat21@clc.fitus.edu.vn>
2023-12-16 00:31:59 +0700

    modified:   FE/app/(tabs)/Playlist.tsx 	modified:   FE/app/(tabs)/index.tsx 	new file:   FE/assets/songs/AMNK.mp3 	new file:   FE/assets/songs/BT.mp3 	new file:   FE/assets/songs/CCNMN.mp3 	new file:   FE/assets/songs/CR.mp3 	new file:   FE/assets/songs/DNDT.mp3 	new file:   FE/assets/songs/EKN.mp3 	new file:   FE/assets/songs/KTD.mp3 	new file:   FE/assets/songs/LNDF.mp3 	new file:   FE/assets/songs/MCCG.mp3 	new file:   FE/assets/songs/NACE.mp3 	new file:   FE/assets/songs/NDDY.mp3 	new file:   FE/assets/songs/NE.mp3 	new file:   FE/assets/songs/R.mp3 	new file:   FE/assets/songs/RMGT.mp3 	new file:   FE/assets/songs/TD.mp3 	new file:   FE/assets/songs/TPS.mp3 	new file:   FE/assets/songs/TTD.mp3 	new file:   FE/assets/songs/TTLLNDCE.mp3 	renamed:    FE/assets/testsong.mp3 -> FE/assets/songs/testsong.mp3 	modified:   FE/package-lock.json 	modified:   FE/package.json

--- FE/app/(tabs)/Playlist.tsx
+++ FE/app/(tabs)/Playlist.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {Ionicons} from '@expo/vector-icons'
-import {Text, View, TextInput, Image, TouchableOpacity, FlatList, ScrollView, Pressable, ActivityIndicator,} from 'react-native'
-import styles from '../styles'
+import {Text, View, TextInput, Image, TouchableOpacity, FlatList, Pressable, ActivityIndicator,} from 'react-native'
+import { ScrollView } from 'react-native-virtualized-view'
 import { Link } from 'expo-router'
 import {categories, playlists, artists, songs} from "../../components/components"
 import Song from '../../components/song'
@@ -9,6 +9,29 @@ import { debounce } from "lodash";
 import { Audio } from 'expo-av';
 
 const PlaylistsScreen = () => {
+  var song_name = "";
+  function get_song_path(song_name:string) {
+    switch (song_name) {
+      case 'Đưa nhau đi trốn': return require('./../../assets/songs/DNDT.mp3');
+      case 'Nấu ăn cho em': return require('./../../assets/songs/NACE.mp3');
+      case 'Ai muốn nghe không': return require('./../../assets/songs/AMNK.mp3');
+      case 'Tháng tư là lời nói dối của em': return require('./../../assets/songs/TTLLNDCE.mp3');
+      case 'Thành phố sương': return require('./../../assets/songs/TPS.mp3');
+      case 'Chỉ Còn Những Mùa Nhớ': return require('./../../assets/songs/CCNMN.mp3');
+      case 'Riêng một góc trời': return require('./../../assets/songs/RMGT.mp3');
+      case 'Khúc thụy du': return require('./../../assets/songs/KTD.mp3');
+      case 'Như đã dấu yêu': return require('./../../assets/songs/NDDY.mp3');
+      case 'Requiem': return require('./../../assets/songs/R.mp3');
+      case 'Eine kleine Nachtmusik': return require('./../../assets/songs/EKN.mp3');
+      case 'Le nozze di Figaro': return require('./../../assets/songs/LNDF.mp3');
+      case 'Tập thể dục': return require('./../../assets/songs/TTD.mp3');
+      case 'Mẹ chẳng có gì': return require('./../../assets/songs/MCCG.mp3');
+      case 'Bên trái': return require('./../../assets/songs/BT.mp3');
+      case 'Nghe em': return require('./../../assets/songs/NE.mp3');
+      case 'Chuyện rằng': return require('./../../assets/songs/CR.mp3');
+      case 'Thay đổi': return require('./../../assets/songs/TD.mp3');
+    }
+  }
   const [searchedTracks, setSearchedTracks] = useState(songs);
   const [input, setInput] = useState("");
   const debouncedSearch = debounce(handleSearch, 800);
@@ -22,10 +45,10 @@ const PlaylistsScreen = () => {
     debouncedSearch(text);
   };
 
-  const [sound, setSound] = React.useState();
-  async function playSound(song_name:string) {
+  const [sound, setSound] = useState();
+  async function playSound() {
     console.log('Loading Sound');
-    const { sound } = await Audio.Sound.createAsync( require('./../../assets/testsong.mp3')
+    const { sound } = await Audio.Sound.createAsync(get_song_path(song_name)
     );
     setSound(sound);
     console.log('Playing Sound');
@@ -140,7 +163,10 @@ const PlaylistsScreen = () => {
             image: item.image,
             artist: item.artist
           }}} asChild>
-          <Pressable onPress={playSound}>
+          <Pressable onPress={() => {
+            song_name = item.title;
+            sound.unloadAsync();
+            playSound();}}>
             <Song
               name = {item.title}
               image = {item.image}
