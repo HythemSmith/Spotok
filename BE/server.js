7304903f44cd9476dd34c3b51f2d63a4071bbde0
VINH BÙI QUANG <21127723@student.hcmus.edu.vn>
2023-12-20 15:54:27 +0700

    commit

--- BE/server.js
+++ BE/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const db = require('./database/db.js')
 const bodyParser = require('body-parser')
 const route = require('./routes/index.js')
 
+
+app.use(cors());
 // Connect to database
 db.connect()
 app.use(bodyParser.json())
